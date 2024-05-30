65203ee54fbe7777cc31be21dc54332b82571cae
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-05-30 19:45:30 +0300

    py code style fixed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """0. Pascal's Triangle"""
 
+
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal's triangle of n"""
-    
+    """Returns a list of lists of integers
+    representing the Pascal's triangle of n"""
+
     triangle = []
 
     if n > 0:
