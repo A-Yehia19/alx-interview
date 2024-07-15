7b99775f9c46e7b17fc62728fca70d98a4b25193
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-15 15:01:37 +0300

    init run

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,5 +2,13 @@
 """Rotate 2D Matrix"""
 
 
-def rotate_2d_matrix(matrix): 
-    pass
+def rotate_2d_matrix(matrix):
+    """rotate matrix"""
+    n = len(matrix)
+    tmp_matrix = [[-1] * n for _ in range(n)]
+
+    for y in range(n):
+        for x in range(n):
+            tmp_matrix[x][y] = matrix[n-y-1][x]
+
+    matrix[:] = tmp_matrix
