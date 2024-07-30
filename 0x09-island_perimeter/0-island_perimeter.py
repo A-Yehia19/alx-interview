65ac62fc8a3e4a627c4d0a81e8619b3478243987
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-30 12:36:27 +0300

    test case

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,35 +2,22 @@
 """Island Perimeter"""
 
 
-def expand_island(grid, row, col):
-    """expand the island"""
-    up = 0 if row - 1 < 0 else grid[row - 1][col]
-    down = 0 if row + 1 < len(grid) else grid[row + 1][col]
-    left = 0 if col - 1 < 0 else grid[row][col - 1]
-    right = 0 if col + 1 < len(grid[0]) else grid[row][col + 1]
-
-    grid[row][col] = -1
-    count = [up, down, left, right].count(0)
-
-    if up == 1:
-        count += expand_island(grid, row - 1, col)
-    if down == 1:
-        count += expand_island(grid, row + 1, col)
-    if left == 1:
-        count += expand_island(grid, row, col - 1)
-    if right == 1:
-        count += expand_island(grid, row, col + 1)
-
-    return count
-
-
 def island_perimeter(grid):
     """find the perimeter of island"""
     perimeter = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 1:
-                perimeter = expand_island(grid, row, col)
-                return perimeter
-
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
