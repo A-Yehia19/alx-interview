e862e19538ec1818e24b360ccef14749084fcf50
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-08-05 15:25:08 +0300

    added changes

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -23,6 +23,7 @@ def isWinner(x, nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
         bens_wins += primes_count % 2 == 0
         marias_wins += primes_count % 2 == 1
+
     if marias_wins == bens_wins:
         return None
     return 'Maria' if marias_wins > bens_wins else 'Ben'
